52092fb4b34a90b714e9b5df8f5868b0c8e0a397
Santosh Basragan <112722023+Sbsantosh8@users.noreply.github.com>
2023-12-14 11:25:28 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,23 @@
+
+var bulb = document.querySelector("#bulb")
+var btn = document.querySelector("#button")
+var flag = 0
+
+
+btn.addEventListener("click",function(){
+if(flag == 0){
+   bulb.style.backgroundColor = "yellow"
+   
+   flag = 1
+btn.innerHTML = "OFF"
+}else{
+   bulb.style.backgroundColor = "white"
+   btn.innerHTML = "ON"
+   flag = 0
+}
+
+
+
+})
+
+
